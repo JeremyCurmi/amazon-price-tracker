fd4d7c04600df6502998efbcad054c0b3f369856
JeremyCurmi <jeremycurmi13@gmail.com>
2021-11-25 08:49:10 +0100

    added support for multiple links, fixed send email logic, added logging system, added fallback when price or shipping price is not found

--- utils.py
+++ utils.py
@@ -1,2 +1,20 @@
+import logging
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+
+
 def has_price_decreased(price: float, max_price: float):
-    return max_price < price
+    return price < max_price
+
+
+def log_service_logic(price: float, max_price: float, send_email: bool):
+    if send_email:
+        logging.info(
+            f"Price: {price} is less than the set threshold: {max_price}!, Sending mail"
+        )
+    else:
+        logging.info(f"Price: {price} is not less than the set threshold: {max_price}")
+
+
+def log_error_get_price():
+    logging.warning("Could not get price!")
